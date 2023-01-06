fc47647f0aaf02c9220143c885bdfca019a1025d
saw-code <saw.coding@gmail.com>
2023-01-06 12:51:49 +0300

    add comment for void function

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 type ButtonType = {
   name: string
-  callBack: ()=> void
+  callBack: ()=> void // это означает что типом будет функция которая ничего не возвращает(void)
 }
 export function Button(props: ButtonType) {
   const onClickHandler = ()=> {
